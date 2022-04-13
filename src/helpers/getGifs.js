f5c5829175d8f845cd75e89d698baf6f0e909c5b
DavidRomeroC <romerocandelariodavid@gmail.com>
2022-04-13 03:05:18 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+
+
+
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=oRWVRoFU8r3ZEN8qqeFi3E2rZFUsqhF0`
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    
+    return gifs;
+}
